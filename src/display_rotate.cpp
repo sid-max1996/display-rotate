a95f79222e2b9848b020150c8e96235cb24168c1
sid-max1996 <sid-max1996@yandex.ru>
2022-12-15 16:21:22 +0400

    feat: add rotate by position

--- src/display_rotate.cpp
+++ src/display_rotate.cpp
@@ -88,10 +88,100 @@ Napi::Boolean display_rotate::rotateByDisplayNumberWrapped(const Napi::CallbackI
   return Napi::Boolean::New(env, returnValue);
 }
 
+bool display_rotate::rotateByDisplayPosition(int orientation, int x, int y) {
+    bool result = false;
+    DISPLAY_DEVICE d;
+    DEVMODE dm;
+    d.cb = sizeof(DISPLAY_DEVICE);
+
+    int index = 0;
+    while (true) {
+        if (!EnumDisplayDevices(NULL, index, &d, EDD_GET_DEVICE_INTERFACE_NAME)) {
+            break;
+        }
+        index += 1;
+
+        if (0 != EnumDisplaySettings(d.DeviceName, ENUM_CURRENT_SETTINGS, &dm))
+        {
+            if (dm.dmPosition.x == x && dm.dmPosition.y == y) {
+                if ((dm.dmDisplayOrientation + (int)orientation) % 2 == 1)
+                {
+                    int temp = dm.dmPelsHeight;
+                    dm.dmPelsHeight = dm.dmPelsWidth;
+                    dm.dmPelsWidth = temp;
+                }
+
+                switch (orientation)
+                {
+                    case DEGREES_CW_90:
+                        dm.dmDisplayOrientation = DMDO_270;
+                        break;
+                    case DEGREES_CW_180:
+                        dm.dmDisplayOrientation = DMDO_180;
+                        break;
+                    case DEGREES_CW_270:
+                        dm.dmDisplayOrientation = DMDO_90;
+                        break;
+                    case DEGREES_CW_0:
+                        dm.dmDisplayOrientation = DMDO_DEFAULT;
+                        break;
+                    default:
+                        break;
+                }
+
+                LONG ret = ChangeDisplaySettingsEx(d.DeviceName, &dm, NULL, CDS_UPDATEREGISTRY, NULL);
+
+                result = ret == 0;
+                break;
+            }
+        }
+     }
+
+    return result;
+}
+
+Napi::Boolean display_rotate::rotateByDisplayPositionWrapped(const Napi::CallbackInfo& info) 
+{
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 3) {
+    Napi::TypeError::New(env, "Two params expected orientation, x, y").ThrowAsJavaScriptException();
+  }
+
+  if (!info[0].IsNumber() || !info[1].IsNumber() || !info[2].IsNumber()) {
+    Napi::TypeError::New(env, "orientation or x or y not a number").ThrowAsJavaScriptException();
+  } 
+
+  Napi::Number first = info[0].As<Napi::Number>();
+  Napi::Number second = info[1].As<Napi::Number>();
+  Napi::Number third = info[2].As<Napi::Number>();
+
+  int orientation = first.Int32Value();
+  int x = second.Int32Value();
+  int y = third.Int32Value();
+
+  if (orientation != 0 && orientation != 90 && orientation != 180 && orientation != 270) {
+    Napi::TypeError::New(env, "orientation must be 0 or 90 or 180 or 270").ThrowAsJavaScriptException();
+  }
+
+  int orientationParam = DEGREES_CW_0;
+  if (orientation == 90) orientationParam = DEGREES_CW_90;
+  if (orientation == 180) orientationParam = DEGREES_CW_180;
+  if (orientation == 270) orientationParam = DEGREES_CW_270;
+
+  bool returnValue = display_rotate::rotateByDisplayPosition(orientationParam, x, y);
+  
+  return Napi::Boolean::New(env, returnValue);
+}
+
 Napi::Object display_rotate::Init(Napi::Env env, Napi::Object exports) 
 {
   exports.Set(
     "rotateByDisplayNumber", Napi::Function::New(env, display_rotate::rotateByDisplayNumberWrapped)
+  );
+
+  exports.Set(
+    "rotateByDisplayPosition", Napi::Function::New(env, display_rotate::rotateByDisplayPositionWrapped)
   );
  
   return exports;
--- src/display_rotate.h
+++ src/display_rotate.h
@@ -3,5 +3,9 @@
 namespace display_rotate {
   bool rotateByDisplayNumber(int displayNumber, int orientation);
   Napi::Boolean rotateByDisplayNumberWrapped(const Napi::CallbackInfo& info);
+
+  bool rotateByDisplayPosition(int orientation, int x, int y);
+  Napi::Boolean rotateByDisplayPositionWrapped(const Napi::CallbackInfo& info);
+
   Napi::Object Init(Napi::Env env, Napi::Object exports);
 }
