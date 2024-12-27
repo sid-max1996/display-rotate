a249423e8c974795cad72c551b551c341c967458
sid-max1996 <sid-max1996@yandex.ru>
2024-12-27 12:44:48 +0400

    v1.2.2 prebuilds

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const displayRotate = require('./build/Release/display-rotate.node')
-module.exports = displayRotate
+// const displayRotate = require('./build/Release/display-rotate.node')
+// module.exports = displayRotate
+
+// Will load a compiled build if present or a prebuild.
+// If no build if found it will throw an exception
+const binding = require("node-gyp-build")(__dirname);
+module.exports = binding;
